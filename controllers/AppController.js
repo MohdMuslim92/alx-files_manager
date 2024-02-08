aaf5aa76efd7f9163586c3c2d4ce577f54b777e7
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-09 01:29:09 +0300

    Implement endpoint handlers in AppController.js

---
+++ controllers/AppController.js
@@ -0,0 +1,18 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+const AppController = {
+  async getStatus (req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
+  },
+
+  async getStats (req, res) {
+    const numUsers = await dbClient.nbUsers();
+    const numFiles = await dbClient.nbFiles();
+    res.status(200).json({ users: numUsers, files: numFiles });
+  }
+};
+
+export default AppController;
