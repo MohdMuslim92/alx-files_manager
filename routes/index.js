7bc405ea5f25de4bec1ee36f64b0a7ded0b61e83
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-09 01:38:43 +0300

    Add POST /users endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 export default router;
