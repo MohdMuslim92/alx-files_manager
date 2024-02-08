1102c6ec17143cafc2838dd3985c16d186064b94
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-09 02:58:54 +0300

    Added routes for GET /connect, GET /disconnect, GET /users/me

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
