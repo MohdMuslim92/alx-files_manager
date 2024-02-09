3b9f3712bbb22d38fed17264b1e2292c77949ed5
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-09 16:39:51 +0300

    Added userQueue for processing background jobs to send welcome emails to users

--- worker.js
+++ worker.js
@@ -3,6 +3,29 @@ import thumbnail from 'image-thumbnail';
 import fs from 'fs';
 import dbClient from './utils/db';
 
+// Create a new queue for processing user jobs
+const userQueue = new Queue('userQueue');
+
+userQueue.process(async (job) => {
+  const { userId } = job.data;
+
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
+
+  const user = await dbClient.db.collection('users').findOne({ _id: userId });
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  console.log(`Welcome ${user.email}!`);
+});
+
+// Export the userQueue instance
+export { userQueue };
+
+// Create a new queue for processing file jobs
 const fileQueue = new Queue('fileQueue');
 
 fileQueue.process(async (job) => {
@@ -37,4 +60,5 @@ fileQueue.process(async (job) => {
   await Promise.all(promises);
 });
 
+// Export the fileQueue instance
 export default fileQueue;
