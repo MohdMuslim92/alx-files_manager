7f6af21036c75a0cda8d159e87f82f2175cbea85
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-10 18:10:14 +0300

    Edit code style to follow eslint guidelines

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,17 +2,17 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 const AppController = {
-  async getStatus (req, res) {
+  async getStatus(req, res) {
     const redisAlive = redisClient.isAlive();
     const dbAlive = dbClient.isAlive();
     res.status(200).json({ redis: redisAlive, db: dbAlive });
   },
 
-  async getStats (req, res) {
+  async getStats(req, res) {
     const numUsers = await dbClient.nbUsers();
     const numFiles = await dbClient.nbFiles();
     res.status(200).json({ users: numUsers, files: numFiles });
-  }
+  },
 };
 
 export default AppController;
